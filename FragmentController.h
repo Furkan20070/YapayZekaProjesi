76d2e4c53e385680ce5815df3936644e751cb9c6
Furkan Ali Turan <126661211+Furkan20070@users.noreply.github.com>
2024-02-06 12:44:55 +0300

    Bounding box çizimi kodu eklendi. Ayrıca Vision Fragment'lere artık çarpraz komşu ataması yapılıyor.

--- FragmentController.h
+++ FragmentController.h
@@ -19,6 +19,7 @@ class FragmentController
 	std::vector<VisionFragment> fragments;
 public:
     vector<VisionFragment*> suspects; //Bu, recursive algoritmada benzer olmayan fragment'lerin adreslerinin saklandığı vektör.
+    vector<vector<int>> intensityMap; //Bulduğumuz objeler, kenarlar ve renk kümelerini matematiksel olarak ifade ettiğimiz değişken.
     vector<Scalar> debugColors; //debug
     int divider = 10; //Vision Fragment objelerinin piksel olarak büyüklüğü (hepsi kare).
     FragmentController(int divider)
@@ -29,7 +30,6 @@ public:
     //Input olarak bir resim alıyoruz. Aldğımız resim üzerinde constructor'da
     //aldığımız divider ile orantılı olarak vision fragment objeleri oluşturuyoruz.
     //YAPILACAK
-    //Şuan neigbour (komşular) sadece üst, alt, sağ ve sol olarak 4 tane atanıyor. Buna çarprazlarıda dahil etmem lazım.
     //Fonksiyonu muhtemelen bölmem gerekiyor (oluştuma dışında ekstra birkaç olay daha var fonksiyon içinde).
     void createFragments(cv::Mat image)
     {
@@ -39,7 +39,7 @@ public:
         debugColors.push_back(Scalar(255, 255, 102));
         debugColors.push_back(Scalar(153, 255, 51));
         debugColors.push_back(Scalar(0, 255, 0));
-        //debugColors.push_back(Scalar(0, 204, 102));
+        debugColors.push_back(Scalar(0, 204, 102));
         debugColors.push_back(Scalar(0, 153, 153));
         //debugColors.push_back(Scalar(0, 51, 102));
         //debugColors.push_back(Scalar(0, 0, 51));
@@ -61,7 +61,7 @@ public:
                 int y = (divider * (i + 1)) - (divider / 2);
                 glm::ivec2 center(x, y);
                 //Fragment oluştur.
-                VisionFragment temp(id, center, divider, image, &suspects, &debugColors);
+                VisionFragment temp(id, center, divider, image, &suspects, &intensityMap, &debugColors);
                 fragments.push_back(temp);
             }
         }
@@ -89,6 +89,23 @@ public:
                 {
                     n.push_back(&fragments[((i + 1) * colAmount) + j]);
                 }
+                //Çarpraz komşular
+                if (i != 0 && j != 0)
+                {
+                    n.push_back(&fragments[((i - 1) * colAmount) + j] - 1);
+                }
+                if (i != 0 && j != colAmount - 1)
+                {
+                    n.push_back(&fragments[((i - 1) * colAmount) + j] + 1);
+                }
+                if (i != rowAmount - 1 && j != 0)
+                {
+                    n.push_back(&fragments[((i + 1) * colAmount) + j] - 1);
+                }
+                if (i != rowAmount - 1 && j != colAmount - 1)
+                {
+                    n.push_back(&fragments[((i + 1) * colAmount) + j] + 1);
+                }
                 //En son olarak Vision Fragment içindeki fonksiyon ile atama işlemi gerçekleştiriyoruz.
                 fragments[(i * colAmount) + j].setNeigbours(n);
             }
@@ -102,7 +119,29 @@ public:
             colorCounter++;
             suspects[0]->startObject(colorCounter);
         }
+        cout << intensityMap[0].size() << endl;
+        drawBoundingBoxes(image);
         cout << "Finished" << endl;
 }
+
+    void drawBoundingBoxes(Mat image)
+    {
+        for (int i = 0; i < intensityMap.size(); i++)
+        {
+            if (intensityMap[i].at(4) > 650)
+            {
+                cout << "drawing" << endl;
+                int minX = intensityMap[i][0] - (divider / 2);
+                int maxX = intensityMap[i][1] + (divider / 2);
+                int minY = intensityMap[i][2] - (divider / 2);
+                int maxY = intensityMap[i][3] + (divider / 2);
+                Point p1(minX, minY);
+                Point p2(maxX, maxY);
+                rectangle(image, p1, p2, Scalar(0, 0, 255), 4);
+                imshow("process", image);
+                waitKey(500);
+            }
+        }
+    }
 };
 #endif
